b158864544264ac2276c767a41a7ad7f51b46924
kateryna-1311 <kattixwak@gmail.com>
2026-04-11 16:29:15 +0300

    Updated third_homework

--- third_homework.py
+++ third_homework.py
@@ -1,50 +1,50 @@
-# #first task "Калькулятор"
+#first task "Калькулятор"
 
-user_maths_operation = "none"
-while user_maths_operation != "Exit":
-    user_first_number = int(input("Enter the first number: "))
-    user_second_number = int(input("Enter the second number: "))
-    user_maths_operation = input("Enter the operation (+, -, *, /, **) or (Exit): ")
 
-    if user_maths_operation == "Exit":
+while True:
+    first_number = int(input("Enter the first number: "))
+    second_number = int(input("Enter the second number: "))
+    arithmetic_operation = input("Enter the operation (+, -, *, /, **) or (Exit): ")
+
+    if arithmetic_operation == "Exit":
         break
-    elif user_maths_operation == "+":
-        result = user_first_number + user_second_number  
-    elif user_maths_operation == "-":
-        result = user_first_number - user_second_number   
-    elif user_maths_operation == "*":
-        result = user_first_number * user_second_number        
-    elif user_maths_operation == "/":
-        result = user_first_number / user_second_number       
-    elif user_maths_operation == "**":
-        result = user_first_number ** user_second_number
-        print(f"Result: {result}")
+    elif arithmetic_operation == "+":
+        result = first_number + second_number  
+    elif arithmetic_operation == "-":
+        result = first_number - second_number   
+    elif arithmetic_operation == "*":
+        result = first_number * second_number        
+    elif arithmetic_operation == "/":
+        if second_number == 0:
+            print("Сan't divide by zero")
+            continue
+        else:
+            result = first_number / second_number       
+    elif arithmetic_operation == "**":
+        result = first_number ** second_number
     else:
         print("Invalid operation. Please enter one of +, -, *, /, **")
+        continue
     print(f"Result: {result}")
 
 #second task "Облік витрат за категоріями"
 
 user_balance = float(input("Enter your balance: "))
-user_costs_by_category = {"Food:": 0, "Games": 0}
-print(user_costs_by_category)
-user_categories = "none"
+expenses_by_category = {"Food:": 0, "Games": 0}
+print(expenses_by_category)
 
-while user_categories != "Exit":
-    user_categories = input("Select the category you spent money on: ").capitalize()
+while True:
+    category = input("Select the category you spent money on: ").capitalize()
 
-    if user_categories == "Exit":
+    if category == "Exit":
         break
-    if user_categories not in user_costs_by_category:
-        user_costs_by_category[user_categories] = 0
-
-    user_amount_of_costs = float(input("Enter the amount of costs: "))
-
-    if user_amount_of_costs > user_balance:
+    
+    expense = float(input("Enter the amount of costs: "))
+    if expense > user_balance:
         print("Insufficient funds")
-    else:
-        user_balance = user_balance - user_amount_of_costs
-        user_costs_by_category[user_categories] += user_amount_of_costs
+        continue
+    user_balance = user_balance - expense
+    expenses_by_category[category] = expenses_by_category.get(category, 0) + expense
 
     print(f"Total balance {user_balance}")
-    print(f"Costs by category: {user_costs_by_category}")
+    print(f"Costs by category: {expenses_by_category}")
